5cc38fb74e048660e10465ecda86d819ab71a4b2
Ian Cheung <ij4.cheung@gmail.com>
2023-09-23 14:18:44 -0400

    copied over from old repo

--- flaskr/app/forms.py
+++ flaskr/app/forms.py
@@ -2,3 +2,42 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, PasswordField, SubmitField, TextAreaField, DateField, RadioField, RadioField, BooleanField, SelectField, SelectMultipleField,EmailField
 from wtforms.validators import DataRequired, StopValidation
 from wtforms.widgets import CheckboxInput, ListWidget
+
+# This is the Registration form for the IT Department Users
+
+class RegisterForm(FlaskForm):
+    Univ_ID = IntegerField('SCSU ID', validators=[DataRequired()], render_kw={"Placeholder" : "SCSU ID"})
+    Birth_Date = DateField('Birth Date',validators=[DataRequired()], render_kw={"placeholder": "Birth Date"})
+    First_Name = StringField('First Name: ',validators=[DataRequired()], render_kw={"placeholder" : "First Name"})
+    Middle_init = StringField('Middle Initial: ', render_kw={})
+    Last_Name = StringField('Last Name: ',validators=[DataRequired()], render_kw={"placeholder" : "Last Name"})
+    email = EmailField('Email: ',validators=[DataRequired()], render_kw={"placeholder" : "Email"})
+    user_name = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})
+    user_password = PasswordField('Password', validators=[DataRequired()],  render_kw={"placeholder": "Password"})
+    submit = SubmitField('Register') 
+
+
+class LoginForm(FlaskForm):
+    user_name = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})
+    password = PasswordField('Password', validators=[DataRequired()],  render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
+
+
+# Reset form will need two forms of Identification to reset passwords both birthday and University ID are required
+class ResetForm(FlaskForm):
+    user_name = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})
+    Univ_ID = IntegerField('SCSU ID', validators=[DataRequired()], render_kw={"Placeholder" : "SCSU ID"})
+    Birth_Date = DateField('Birth Date',validators=[DataRequired()], render_kw={"placeholder": "Birth Date"})
+    submit = SubmitField(label='Reset Password', validators=[DataRequired()])
+
+
+
+
+#WE will need to readdress the loan form we want to start the login process first 
+#    class LoanForm(FlaskForm):
+#    faculty = StringField('Faculty Name: ',validators=[DataRequired()])
+#   device = StringField('Which device do you need? ',validators=[DataRequired()])
+#    is_located = StringField('Device Loca ',validators=[DataRequired()])
+#   loan_Date_In = DateField('When will this device be deployed? ',validators=[DataRequired()])
+#    why = StringField('Why do you need this device? ',validators=[DataRequired()])
+#    submit = SubmitField('Request Loan')
--- flaskr/app/routes.py
+++ flaskr/app/routes.py
@@ -1,9 +1,10 @@
 
 from flask import render_template, redirect, url_for, request
 from app import app
+from app.forms import LoginForm, RegisterForm, ResetForm
 #from app.forms import #RegisterForm
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def go():
     return redirect(url_for('index'))
 
@@ -11,4 +12,36 @@ def go():
 @app.route('/home', methods=['GET','POST'])
 def index():
     return render_template('home.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_name = form.user_name.data
+        password = form.password.data
+        return "user name: " + user_name + "\n Password: " + password
+    return render_template('forms.html', form = form)
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_name = form.user_name.data
+        user_password = form.user_password.data
+        First_Name = form.First_Name.data
+        Middle_init = ""
+        if form.Middle_init.data:
+            Middle_init = form.Middle_init.data
+        Last_Name = form.Last_Name.data
+        email = form.email.data
+        bdate = form.Birth_Date.data
+        uid = form.Univ_ID.data
+
+        response = "user name: " + user_name + "\n Password: " + user_password + "\n First Name: " + First_Name + "\n Last Name: " + Last_Name + "\n Email: " + email + "\n Birth Date: " + str(bdate) + "\n SCSU ID: " + str(uid)
+        if Middle_init:
+            response += "\n Middle Initial: " + Middle_init
+        return response
+    return render_template('register.html', form = form)
 
