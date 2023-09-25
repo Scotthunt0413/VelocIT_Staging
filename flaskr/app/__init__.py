6b12ce2185b8b11ae5f7c59f65915a2e46e61297
Scotthunt0413 <hunts10@southernct.edu>
2023-09-25 01:35:45 +0000

    added flask_sqlalchemy

--- flaskr/app/__init__.py
+++ flaskr/app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'VelocIT'
@@ -7,11 +9,10 @@ app.config['SECRET_KEY'] = 'VelocIT'
 load_dotenv('.flaskenv')
 
 
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 from os import environ
-import mysql.connector
+
 
 # force loading of environment variables
 from dotenv import load_dotenv
