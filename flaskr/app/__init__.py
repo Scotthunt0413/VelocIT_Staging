cce341f79919e4009c51f64cefbed2141a6f3d13
Scotthunt0413 <hunts10@southernct.edu>
2023-09-25 02:00:03 +0000

    fixed __init__

--- flaskr/app/__init__.py
+++ flaskr/app/__init__.py
@@ -3,20 +3,34 @@ from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-from os import environ
-
-load_dotenv('.flaskenv')
-
-IP = environ.get('MYSQL_IP')
-USERNAME = environ.get('MYSQL_USER')
-PASSWORD = environ.get('MYSQL_PASS')
-DB_NAME = environ.get('MYSQL_DB')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'VelocIT'
 
-DB_CONFIG_STR = f'mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{IP}/{DB_NAME}'
+load_dotenv('.flaskenv')
+
+from os import environ
+
+
+# force loading of environment variables
+
+load_dotenv('.flaskenv')
+
+# Get the environment variables from .flaskenv
+IP = environ.get('MYSQL_IP')
+USERNAME = environ.get('MYSQL_USERNAME')
+PASSWORD = environ.get('MYSQL_PASSWORD')
+DB_NAME = environ.get('MYSQL_DBNAME')
+
+# Specify the connection parameters/credentials for the database
+DB_CONFIG_STR = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{IP}/{DB_NAME}"
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONFIG_STR
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+
+# Create database connection and associate it with the Flask application
+db = SQLAlchemy(app)
+
+login = LoginManager(app)
+
 
 
 from app import routes
