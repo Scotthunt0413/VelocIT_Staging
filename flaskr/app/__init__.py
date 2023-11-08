4ebb861d99c03bb165bda9670187db67bd0958b6
Ian Cheung <ij4.cheung@gmail.com>
2023-11-07 22:53:06 -0500

    Revert "getting scott's changes"
    This reverts commit 9b929aeb8344986cb130a9dcd62dcf8afc1bf33d, reversing
    changes made to 0ebcceed843d90fb7061c3416c1499d518957c2c.

--- flaskr/app/__init__.py
+++ flaskr/app/__init__.py
@@ -3,10 +3,9 @@ from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-from os import environ
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'VelocIT'
 
 from os import environ
 
@@ -15,8 +14,6 @@ from os import environ
 
 load_dotenv('.flaskenv')
 
-
-## MIKE WANTS US TO PUT ENVIRONMENT VARIABLES OUTSIDE OF THE WORKING DIRECTORY FOR SECURTIY MEASURES
 # Get the environment variables from .flaskenv
 IP = environ.get('MYSQL_IP')
 USERNAME = environ.get('MYSQL_USERNAME')
@@ -27,7 +24,6 @@ DB_NAME = environ.get('MYSQL_DBNAME')
 DB_CONFIG_STR = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{IP}/{DB_NAME}"
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONFIG_STR
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
-app.config['SECRET_KEY'] = 'VelocIT'
 
 # Create database connection and associate it with the Flask application
 
